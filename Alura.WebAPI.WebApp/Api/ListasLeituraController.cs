3c3aad2edee79c84c446b5d557b2762c51fabccf
Paulo.bressan <Paulo.bressan@outlook.com>
2018-09-19 23:28:05 -0300

    Padrão Rest, Padrão API Asp Net

---
+++ Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
@@ -0,0 +1,50 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Alias, usado para importar um namespace que o mesmo nome existe em uma classe
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListasLeituraController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repository;
+
+        public ListasLeituraController(IRepository<Livro> repository)
+        {
+            _repository = repository;
+        }
+
+        private Lista CriaLista (TipoListaLeitura tipo)
+        {
+            var livros = _repository
+                .All
+                .Where(l => l.Lista == tipo).ToList()
+                .Select(l => l.ToApi());
+            return new Lista
+            {
+                Tipo = tipo.ParaString(),
+                Livros = livros
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Lista paraLer = CriaLista(TipoListaLeitura.ParaLer);
+            Lista lendo = CriaLista(TipoListaLeitura.Lendo);
+            Lista lidos = CriaLista(TipoListaLeitura.Lidos);
+            var colecao = new List<Lista>
+            {
+                paraLer, lendo, lidos
+            };
+            return Ok(colecao);
+        }
+    }
+}
--- Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -8,75 +8,98 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Alura.WebAPI.WebApp.Api
 {
-	public class LivrosController : Controller
-	{
-		private readonly IRepository<Livro> _repository;
-		public LivrosController(IRepository<Livro> repository)
-		{
-			_repository = repository;
-		}
+    //Para identificar que essa classe é uma classe de API, vamos usar o ApiController
+    //Ele auxilia a identificação da classe como uma API
+    [ApiController]
+    //O atributo Route é obrigatório quando identificamos que a classe é uma API
+    [Route("[controller]")]
+    //ControllerBase é especifico para Web Api
+    //Nele não contem recursos para a renderização de Views(Apresentação)
+    public class LivrosController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repository;
+        public LivrosController(IRepository<Livro> repository)
+        {
+            _repository = repository;
+        }
 
-		//Buscar livros por id
-		[HttpGet]
-		public IActionResult Recupera(int id)
-		{
-			var livro = _repository.Find(id);
-			if (livro == null)
-				return NotFound();
-			//toModel Metodo criado para converter o model para um "modelView"
-			return Json(livro.ToModel());
-		}
+        //PADRONIZANDO REST
 
-		//Incluir livro
-		[HttpPost]
-		public IActionResult Incluir(LivroUpload model)
-		{
-			if (ModelState.IsValid)
-			{
-				var livro = model.ToLivro();
-				_repository.Incluir(livro);
-				//O Controller, classe pai do controller livros, tem varios metodos que auxiliam o desenvolvimento WEB
-				//Um deles é a criação de URL com base em uma action, podemos passar parametro em um objeto anonimo.
-				var uri = Url.Action("Recuperar", new { id = livro.Id });
-				return Created(uri, livro);
-			}
-			//Se o codigo não for valido vamos retornar uma requisição invalida
-			return BadRequest();
-		}
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var listaLivros = _repository.All.Select(l => l.ToModel()).ToList();
+            return Ok(listaLivros);
+        }
 
-		//Alterar Livro
-		[HttpPost]
-		public IActionResult Alterar(LivroUpload model)
-		{
-			//Se o modelo for valido
-			if (ModelState.IsValid)
-			{
-				var livro = model.ToLivro();
-				if (model.Capa == null)
-				{
-					livro.ImagemCapa = _repository.All
-						.Where(l => l.Id == livro.Id)
-						.Select(l => l.ImagemCapa)
-						.FirstOrDefault();
-				}
-				_repository.Alterar(livro);
-				return Ok();
-			}
-			//Senão formato invalido
-			return BadRequest();
-		}
+        //Buscar livros por id
+        //O segundo seguimento ou parametro é o parametro que o metodo espera na URL
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var livro = _repository.Find(id);
+            if (livro == null)
+                return NotFound();
+            //toModel Metodo criado para converter o model para um "modelView"
+            return Ok(livro.ToModel());
+        }
 
-		//Metodo para remover objeto pelo id
-		[HttpPost]
-		public IActionResult Remover(int id)
-		{
-			var model = _repository.Find(id);
-			if (model == null)
-			{
-				return NotFound();
-			}
-			_repository.Excluir(model);
-			return RedirectToAction("Index", "Home");
-		}
-	}
+        //Incluir livro
+        //Para definir que o parametro esta vindo no corpo da requisição
+        //vamos utilizar o FromBody
+        [HttpPost]
+        public IActionResult Post([FromBody]LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repository.Incluir(livro);
+                //O Controller, classe pai do controller livros, tem varios metodos que auxiliam o desenvolvimento WEB
+                //Um deles é a criação de URL com base em uma action, podemos passar parametro em um objeto anonimo.
+                var uri = Url.Action("Get", new { id = livro.Id });
+                return Created(uri, livro);
+            }
+            //Se o codigo não for valido vamos retornar uma requisição invalida
+            return BadRequest();
+        }
+
+        //Alterar Livro
+        //Para definir que o parametro esta vindo no corpo da requisição
+        //vamos utilizar o FromBody
+        [HttpPut]
+        public IActionResult Put([FromBody]LivroUpload model)
+        {
+            //Se o modelo for valido
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repository.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repository.Alterar(livro);
+                return Ok();
+            }
+            //Senão formato invalido
+            return BadRequest();
+        }
+
+        //Metodo para remover objeto pelo id
+        //O metodo delete espera como parametro o id, para isso definimos como segundo
+        //segmento o id como parametro na URL
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var model = _repository.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repository.Excluir(model);
+            return NoContent();
+        }
+    }
 }
